b1646b0256c2204e24dcc595e81be1a8fa76bf32
MAXSHA256 <38231861+MAXSHA256@users.noreply.github.com>
2019-04-21 16:34:19 -0400

    Update Power of Two.py

--- Bit manipulation/Power of Two.py
+++ Bit manipulation/Power of Two.py
@@ -1,3 +1,4 @@
+'''
 **231. Power of Two**
 
 Given an integer, write a function to determine if it is a power of two.
@@ -18,7 +19,7 @@ Input: 218
 Output: false
 
 最开始的想法: 用recursion
-```
+'''
 class Solution:
     def isPowerOfTwo(self, n) -> bool:
         if n != int(n): # not whole number
@@ -29,4 +30,4 @@ class Solution:
             return self.isPowerOfTwo(n/2)
         else:
             return False  
-```
+
